44bfecb05e451168a896bfc9cf5089b6af3155a8
21FTT1290 <98886596+21FTT1290@users.noreply.github.com>
2023-04-26 09:36:08 +0800

    added shahmi's parts (payment page)

---
+++ assignment/cashpay.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" />
+    <title>Document</title>
+
+    <style>
+        * {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v143_16 {
+  width: 100%;
+  height: 960px;
+  background: rgba(199,255,237,1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v143_17 {
+  width: 100%;
+  height: 878px;
+  background: rgba(28,130,173,1);
+  opacity: 1;
+  position: absolute;
+  top: 35px;
+  left: 54px;
+  border-top-left-radius: 180px;
+  border-top-right-radius: 180px;
+  border-bottom-left-radius: 180px;
+  border-bottom-right-radius: 180px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v143_29 {
+  width: 258px;
+  height: 90px;
+  background: rgba(100,212,105,1);
+  opacity: 1;
+  position: absolute;
+  top: 707px;
+  left: 591px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.v143_27 {
+  width: 179px;
+  height: 179px;
+  background: url("../images/v143_27.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 152px;
+  left: 631px;
+  overflow: hidden;
+}
+.v143_18 {
+  width: 179px;
+  height: 179px;
+  background: rgba(100,212,104,1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border-radius: 50%;
+  box-shadow: 0px 14px 39px rgba(0, 0, 0, 0.25);
+}
+.v143_19 {
+  width: 94px;
+  height: 59px;
+  background: url("../images/v143_19.png");
+  opacity: 1;
+  position: absolute;
+  top: 60px;
+  left: 45px;
+  border: 4px solid rgba(255,255,255,1);
+}
+.v143_20 {
+  width: 100%;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 403px;
+  left: 152px;
+  font-family: Inter;
+  font-weight: Bold;
+  font-size: 50px;
+  opacity: 1;
+  text-align: center;
+}
+.v143_21 {
+  width: 100%;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 494px;
+  left: 152px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 26px;
+  opacity: 1;
+  text-align: center;
+}
+.v143_31 {
+  width: 258px;
+  color: rgba(255,255,255,1);
+  position: absolute;
+  top: 727px;
+  left: 591px;
+  font-family: Inter;
+  font-weight: Regular;
+  font-size: 32px;
+  opacity: 1;
+  text-align: center;
+}
+
+    </style>
+
+</head>
+
+<body>
+    <div class="v143_16">
+        <div class="v143_17"></div>
+        <div class="v143_29"></div>
+        <div class="v143_27">
+            <div class="v143_18"></div>
+            <div class="v143_19"></div>
+        </div><span class="v143_20">Please pay cash by the counter.</span><span class="v143_21">Thank you for booking
+            (name).
+            Your booking details have been Emailed to you.</span><button class="v143_31">Okay</button>
+    </div>
+</body>
+
+</html>
