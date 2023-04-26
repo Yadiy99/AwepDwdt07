4dae27f601f94bb13a9765f9793e0e9328aad80a
21FTT1290 <98886596+21FTT1290@users.noreply.github.com>
2023-04-26 09:38:44 +0800

    added shahmi's main payment page

---
+++ assignment/paypage.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="./css/main.css" rel="stylesheet" />
+    <title>Document</title>
+
+    <style>
+        * {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v143_3 {
+  width: 100%;
+  height: 960px;
+  background: rgba(199,255,237,1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v143_4 {
+  width: 100%;
+  height: 764px;
+  background: rgba(255,255,255,1);
+  opacity: 1;
+  position: absolute;
+  top: 155px;
+  left: 53px;
+  border-top-left-radius: 63px;
+  border-top-right-radius: 63px;
+  border-bottom-left-radius: 63px;
+  border-bottom-right-radius: 63px;
+  overflow: hidden;
+}
+.v143_7 {
+  width: 734px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 45px;
+  left: 353px;
+  font-family: Inter;
+  font-weight: Bold;
+  font-size: 55px;
+  opacity: 1;
+  text-align: center;
+}
+.v143_23 {
+  width: 414px;
+  height: 417px;
+  background: url("../images/cash.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 338px;
+  left: 198px;
+  overflow: hidden;
+}
+.v143_5 {
+  width: 414px;
+  height: 283px;
+  background: url("../images/cash.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v143_8 {
+  width: 393px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 313px;
+  left: 10px;
+  font-family: Inter;
+  font-weight: Bold;
+  font-size: 60px;
+  opacity: 1;
+  text-align: center;
+}
+.v143_24 {
+  width: 414px;
+  height: 447px;
+  background: url("../images/card.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 308px;
+  left: 827px;
+  overflow: hidden;
+}
+.v143_6 {
+  width: 414px;
+  height: 343px;
+  background: url("../images/card.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v143_9 {
+  width: 393px;
+  color: rgba(0,0,0,1);
+  position: absolute;
+  top: 343px;
+  left: 10px;
+  font-family: Inter;
+  font-weight: Bold;
+  font-size: 60px;
+  opacity: 1;
+  text-align: center;
+}
+
+    </style>
+
+</head>
+
+<body>
+    <div class="v143_3">
+        <div class="v143_4"></div><span class="v143_7">Type of payment</span>
+        <div class="v143_23">
+            <div class="v143_5">
+            <img src="images/cash.png">
+            </div>
+            <button class="v143_8">Cash</button>
+        </div>
+        <div class="v143_24">
+            <div class="v143_6">
+            <img src="images/card.png">
+            </div>
+            <button class="v143_9">Card</button>
+        </div>
+    </div>
+</body>
+
+</html>
