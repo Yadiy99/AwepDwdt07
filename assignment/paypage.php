9f8835d76fa7f0c9a5a2b6b99cc98ed024db7e90
21FTT1290 <98886596+21FTT1290@users.noreply.github.com>
2023-05-16 16:20:04 +0800

    Update paypage.php entirely

--- assignment/paypage.php
+++ assignment/paypage.php
@@ -8,142 +8,99 @@
 
     <style>
         * {
-  box-sizing: border-box;
-}
-body {
-  font-size: 14px;
-}
-.v143_3 {
-  width: 100%;
-  height: 960px;
-  background: rgba(199,255,237,1);
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
-.v143_4 {
-  width: 100%;
-  height: 764px;
-  background: rgba(255,255,255,1);
-  opacity: 1;
-  position: absolute;
-  top: 155px;
-  left: 53px;
-  border-top-left-radius: 63px;
-  border-top-right-radius: 63px;
-  border-bottom-left-radius: 63px;
-  border-bottom-right-radius: 63px;
-  overflow: hidden;
-}
-.v143_7 {
-  width: 734px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 45px;
-  left: 353px;
-  font-family: Inter;
-  font-weight: Bold;
-  font-size: 55px;
-  opacity: 1;
-  text-align: center;
-}
-.v143_23 {
-  width: 414px;
-  height: 417px;
-  background: url("../images/cash.png");
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 338px;
-  left: 198px;
-  overflow: hidden;
-}
-.v143_5 {
-  width: 414px;
-  height: 283px;
-  background: url("../images/cash.png");
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
-.v143_8 {
-  width: 393px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 313px;
-  left: 10px;
-  font-family: Inter;
-  font-weight: Bold;
-  font-size: 60px;
-  opacity: 1;
-  text-align: center;
-}
-.v143_24 {
-  width: 414px;
-  height: 447px;
-  background: url("../images/card.png");
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 308px;
-  left: 827px;
-  overflow: hidden;
-}
-.v143_6 {
-  width: 414px;
-  height: 343px;
-  background: url("../images/card.png");
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  opacity: 1;
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  overflow: hidden;
-}
-.v143_9 {
-  width: 393px;
-  color: rgba(0,0,0,1);
-  position: absolute;
-  top: 343px;
-  left: 10px;
-  font-family: Inter;
-  font-weight: Bold;
-  font-size: 60px;
-  opacity: 1;
-  text-align: center;
-}
+            box-sizing: border-box;
+        }
 
+        body {
+            font-size: 14px;
+        }
+
+        .container {
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: rgba(199, 255, 237, 1);
+        }
+
+        .payment-options {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 30px;
+            padding: 20px;
+            background-color: rgba(255, 255, 255, 1);
+            border-radius: 30px;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+        }
+
+        .payment-title {
+            font-family: Inter;
+            font-weight: bold;
+            font-size: 30px;
+            text-align: center;
+        }
+
+        .payment-option {
+            align-items: center;
+            gap: 10px;
+            text-align: center;
+        }
+
+        .payment-icon {
+            width: 150px;
+            height: 150px;
+            background-size: cover;
+            background-position: center;
+        }
+
+        .payment-button {
+            width: 400px;
+            padding: 10px;
+            font-size: 18px;
+            font-weight: bold;
+            color: #ffffff;
+            border: none;
+            border-radius: 30px;
+            background-color: #0088cc;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        .payment-button:hover {
+            background-color: #006699;
+        }
+
+        img{
+          width: 180px;
+          height: 180px;
+        }
     </style>
-
 </head>
 
 <body>
-    <div class="v143_3">
-        <div class="v143_4"></div><span class="v143_7">Type of payment</span>
-        <div class="v143_23">
-            <div class="v143_5">
-            <img src="assets/cash.png">
+    <div class="container">
+        <div class="payment-options">
+            <h1 class="payment-title">Choose a Payment Method</h1>
+            <div class="payment-option">
+                
+              <button class="payment-button">
+                <img src="images/cash.png" alt="Cash Icon" />
+                <br>
+                Cash
+              </button>
+
             </div>
-            <button class="v143_8">Cash</button>
-        </div>
-        <div class="v143_24">
-            <div class="v143_6">
-            <img src="assets/card.png">
+            <div class="payment-option">
+
+            <button class="payment-button">
+                <img src="images/card.png" alt="Card Icon" />
+                <br>
+                Card
+            </button>
+
             </div>
-            <button class="v143_9">Card</button>
         </div>
     </div>
 </body>
